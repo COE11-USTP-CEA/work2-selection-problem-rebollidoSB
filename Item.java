bb85ef6ff2e84c7cd8b64291ce1f745e5a298e62
rebollidoSB <oninrebollido@yahoo.com>
2017-01-13 05:58:53 -0800

    Item.java

---
+++ Item.java
@@ -0,0 +1,15 @@
+ublic class item {
+    
+    String item_name;
+    String c_name;
+    float price;
+    
+    item(String name, String category, float p){
+        
+        item_name = name;
+        c_name = category;
+        price = p;
+        
+    }
+    
+}
